c928364407c7a5c0155ab78c1b583945ca632561
Sharath Bhattiprolu <sharathjb23@gmail.com>
2026-09-21 08:33:45 -0700

    feat: add AMQP 1.0 supported source options
    Signed-off-by: Sharath Bhattiprolu <sharathjb23@gmail.com>

---
+++ internal/provider/connectors/amqp10/amqp10.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2026, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package amqp10
+
+var supportedSourceOptions = map[string]bool{
+	"MessageTTL":        true,
+	"DeadLetterAddress": true,
+	"DeadLetterSubject": true,
+	"Expires":           true,
+	"Offset":            true,
+	"ConsumerGroup":     true,
+}
+
+// TODO - Issue 187 - Implement the remaining Provider methods and register the AMQP 1.0 provider.
+type amqp10provider struct{}
+
+// NewAMQP10Provider returns an AMQP 1.0 provider instance.
+func NewAMQP10Provider() *amqp10provider {
+	return &amqp10provider{}
+}
+
+// SupportedSourceOptions returns the source options supported by AMQP 1.0.
+func (prov *amqp10provider) SupportedSourceOptions() map[string]bool {
+	return supportedSourceOptions
+}
---
+++ internal/provider/connectors/amqp10/amqp10_test.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2026, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package amqp10
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_SupportedSourceOptions(t *testing.T) {
+	prov := NewAMQP10Provider()
+	opts := prov.SupportedSourceOptions()
+	assert.NotNil(t, opts)
+	expected := map[string]bool{
+		"MessageTTL":        true,
+		"DeadLetterAddress": true,
+		"DeadLetterSubject": true,
+		"Expires":           true,
+		"Offset":            true,
+		"ConsumerGroup":     true,
+	}
+
+	assert.Equal(t, expected, opts)
+}
